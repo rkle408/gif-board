0ce7022ed4c45eb4ebeed8fc66322a7ed2256c1e
Vivian Chen <vivian.chen1193@gmail.com>
2022-09-13 14:18:46 -0700

    Started on function to get a reponse from giphy api

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 // Giphy API
 var giphyUrl = `api.giphy.com/v1/gifs/search?q=${randomWord}&limit=${numberOfResults}&api_key=${apiKey}`
-// Random word API
+
+function getGiphy () {
+    fetch(giphyUrl)
+        .then (function (response) {
+            response.json();
+        })
+}
+
+
+
+

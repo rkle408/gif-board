ff091965ff3c4f3a815ed5acfd937c3b6583b5b4
Vivian Chen <vivian.chen1193@gmail.com>
2022-09-13 13:53:21 -0700

    added params to giphy url

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,3 @@
 // Giphy API
-var giphyUrl = "api.giphy.com/v1/gifs/search"
+var giphyUrl = `api.giphy.com/v1/gifs/search?q=${randomWord}&limit=${numberOfResults}&api_key=${apiKey}`
 // Random word API

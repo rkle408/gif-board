c6b8338af3e3c3d44102c13b745a9bde3e5c04c7
alexmkinsman <akinsman825@gmail.com>
2022-09-14 12:55:17 -0700

    generate button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 // Giphy API
-var giphyUrl = `api.giphy.com/v1/gifs/search?q=${randomWord}&limit=${numberOfResults}&api_key=${apiKey}`
+// var giphyUrl = `api.giphy.com/v1/gifs/search?q=${randomWord}&limit=${numberOfResults}&api_key=${apiKey}`
 
-function getGiphy () {
-    fetch(giphyUrl)
-        .then (function (response) {
-            if (response) {
-                response.json();
-            } else {
-                console.log(err);
-            }
-        })
-        .then (function (data) {
-            console.log(data);
-        })
-}
+// function getGiphy () {
+//     fetch(giphyUrl)
+//         .then (function (response) {
+//             if (response) {
+//                 response.json();
+//             } else {
+//                 console.log(err);
+//             }
+//         })
+//         .then (function (data) {
+//             console.log(data);
+//         })
+// }
 
 // Random word API
 
@@ -21,15 +21,37 @@ var genBtn = document.getElementById('generatorBtn');
 genBtn.addEventListener('click', hide);
   function hide() {
       var gifGen = document.getElementById('gif-gen');
-      var h2 = document.getElementById('h2-1');
-      if (h2.style.display === 'none') {
-          h2.style.display = 'block'
+      var hideMainEl = document.getElementById('hideSection')
+      genBtn.textContent = "Go Back" ;
+      if (hideMainEl.style.display === 'none') {
+          hideMainEl.style.display = 'block'
           gifGen.style.display = 'none';
+
       } else {
-          h2.style.display = 'none'
+          hideMainEl.style.display = 'none'
           gifGen.style.display = 'block';
 
       }
   
   }
+
+  var genBtn = document.getElementById('generatorBtn');
+  genBtn.addEventListener('click', hide);
+    
+  function hide() {
+        var gifGen = document.getElementById('gif-gen');
+        var hideMainEl = document.getElementById('hideSection')
+        genBtn.textContent = "Go Back" ;
+        if (hideMainEl.style.display === 'none') {
+            hideMainEl.style.display = 'block'
+            
+            gifGen.style.display = 'none';
+  
+        } else {
+            hideMainEl.style.display = 'none'
+            gifGen.style.display = 'block';
+  
+        }
+    
+    }
 
